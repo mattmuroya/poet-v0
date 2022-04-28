63b4998bcfbb613f38bfe5f26d3496f7112150db
mattmuroya <matt.muroya@gmail.com>
2022-04-27 18:39:08 -1000

    add mongoose setup and User model

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { MONGODB_URL } = require('./utils/config');
 
-// DB CONNECT
+(async () => {
+  try {
+    await mongoose.connect(MONGODB_URL);
+    console.log('MongoDB connected.');
+  } catch (err) {
+    console.log('MongoDB connection error: ', err.message);
+  }
+})();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minlength: 1,
+    maxlength: 32,
+    required: true,
+    unique: true
+  },
+  passwordHash: String
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
