72cc4e5f7b4bad2f6bcc3ea55d28646ae07eeb1e
mattmuroya <matt.muroya@gmail.com>
2022-05-01 21:35:08 -1000

    implement buddy chat GET method and unit tests

--- server/controllers/roomRouter.js
+++ server/controllers/roomRouter.js
@@ -2,30 +2,41 @@ const roomRouter = require('express').Router();
 const Room = require('../models/room');
 
 roomRouter.get('/', async (req, res) => {
-  // const rooms = await Room.find({})
-  //   .populate('users', {
-  //     screenName: 1,
-  //     id: 1
-  // });
-  const rooms = await Room.find({});
+  const rooms = await Room.find({})
+    .populate('users', {
+      screenName: 1,
+      id: 1
+  });
   res.json(rooms);
 });
 
+roomRouter.get('/buddy-chat', async (req, res) => {
+  const { userId, buddyId } = req.body;
+  // may need to add buddy chat property to differentiate from group chatroom
+  const buddyRoom = await Room.findOne({
+    users: { $all: [userId, buddyId]
+  }});
+  console.log(buddyRoom);
+  res.json(buddyRoom);
+});
+
+// roomRouter.get('/group-chat' async (req, res) => {
+//   res.json('not yet implemented');
+// });
+
 roomRouter.post('/', async (req, res) => {
   const { users } = req.body;
-
   const room = new Room({
     users
   });
-
   const savedRoom = await room.save();
   res.status(201).json(savedRoom);
 });
 
-roomRouter.get('/:id', async (req, res) => {
-  const room = await Room.findById(req.params.id);
-  res.json(room);
-  console.log(room);
-})
+// roomRouter.get('/:id', async (req, res) => {
+//   const room = await Room.findById(req.params.id);
+//   res.json(room);
+//   console.log(room);
+// })
 
 module.exports = roomRouter;
--- server/tests/api.rooms.test.js
+++ server/tests/api.rooms.test.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Room = require('../models/room');
 const api = supertest(app);
+
+const User = require('../models/user');
 
 const { resetTestDb } = require('./utils');
 
 beforeAll(resetTestDb);
-
-test('dummy test', () => {
-  expect(0).toBe(0);
-})
 
 describe('getting rooms', () => {
   test('can get full list of rooms', async () => {
@@ -17,6 +16,21 @@ describe('getting rooms', () => {
       .expect(200);
     expect(res.body).toHaveLength(1);
     expect(res.body[0].users).toHaveLength(2);
+  });
+
+  test('can get room by user and buddy ids', async () => {
+    const user = await User.findOne({ screenName: 'admin' });
+    const buddy = await User.findOne({ screenName: 'root' });
+    // currently there's only one room document in the test DB.
+    // may need to refactor if test DB expands in the future.
+    const room = await Room.findOne({})
+    const res = await api.get('/api/rooms/buddy-chat')
+      .send({
+        userId: user._id.toString(),
+        buddyId: buddy._id.toString()
+      })
+      .expect(200);
+    expect(res.body.id).toBe(room._id.toString());
   });
 });
 
